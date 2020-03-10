9743c79a3c9c271e7c757fa955999df4215ab013
Zbigniew Downar <zbyszrom@Zbigniews-iMac.local>
2020-03-10 19:17:23 +0100

    background image

--- src/components/showcase.js
+++ src/components/showcase.js
@@ -1,16 +1,34 @@
 import React from 'react'
 import {Link} from 'gatsby'
+import { useStaticQuery, graphql } from "gatsby"
+import BackgroundImage from 'gatsby-background-image'
+
+const getImage = graphql`
+query {
+    Bcg: file(name: {eq: "featured"}) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+}
+`
 
 
 const Showcase = ()=> {
+
+    const data = useStaticQuery(getImage)
+
     return(
-        <header>
+        <BackgroundImage
+        fluid = {data.Bcg.childImageSharp.fluid} className = ''>
             <div className =''>
             <h1>Some Sports Article</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus rerum officia quibusdam mollitia deserunt animi soluta laudantium. Quam sapiente a dolorum magnam necessitatibus quis tempore facere totam. Dolor, sequi distinctio!</p>
             <Link to = ''> Read more</Link>
             </div>
-        </header>
+          </BackgroundImage>  
 
     )
 }
