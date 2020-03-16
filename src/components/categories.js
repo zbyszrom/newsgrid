bb1c049df53e55f7f8cb3f4e2741dd84ce84e495
Zbigniew Downar <zbyszrom@Zbigniews-iMac.local>
2020-03-16 15:34:09 +0100

    categories

--- src/components/categories.js
+++ src/components/categories.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import {FaChevronRight} from 'react-icons/fa' 
 
 const Categories = ()=>{
     return(
+        <aside>
         <h2>Categories</h2>
+        <ul >
+            <li><a href="#"><FaChevronRight/>Sports</a></li>
+            <li><a href="#"><FaChevronRight/>Entertainment</a></li>
+            <li><a href="#"><FaChevronRight/>Technology</a></li>
+            <li><a href="#"><FaChevronRight/>Fashion</a></li>
+            <li><a href="#"><FaChevronRight/>Shopping</a></li>
+          </ul>
+          </aside>
     )
 }
 export default Categories
